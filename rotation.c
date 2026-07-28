a3983442802838834a9a5077c71a34f98e1342c0
kamleshyadav769 <yadavkamlesh5251@gmail.com>
2026-07-28 20:40:48 +0530

    Refactor code structure for improved readability and maintainability

--- rotation.c
+++ rotation.c
@@ -114,7 +114,7 @@ struct Node *RInsert(struct Node *p,int key){
 p->height=NodeHeight(p);
 if(BalanceFactor(p)==2&&BalanceFactor(p->lchild)==1){
 
-     printf("ll apllied");
+     printf("ll apllied\n");
     return LLRotation(p);
    
 }
@@ -135,21 +135,161 @@ else if(BalanceFactor(p)==-2&&BalanceFactor(p->rchild)==1){
  return p;
 }
 
+struct Node*InPre(struct Node*p){
+    while(p&&p->rchild){
+        p=p->rchild;
+    }
+    return p;
+}
+struct Node*InSucc(struct Node*p){
+    while(p&&p->lchild){
+        p=p->lchild;
+    }
+    return p;
+}
+
+struct Node *RDelete(struct Node *p,int key){
+    struct Node*q;
+    if(p==NULL){
+        return NULL;
+    }
+if(p->lchild==NULL && p->rchild==NULL ){
+    if(p==root){
+        root=NULL;
+    }
+    free(p);
+    return NULL;
+}
+
+ if(key<p->data){
+     p->lchild=RDelete(p->lchild,key);
+ }
+ else if(key>p->data) {
+     p->rchild=RDelete(p->rchild,key);
+ } 
+ else{
+    
+    if(NodeHeight(p->lchild)>NodeHeight(p->rchild)){
+        q=InPre(p->lchild);
+        p->data=q->data;
+         p->lchild=RDelete(p->lchild,q->data); 
+    }else{
+         q=InSucc(p->rchild);
+        p->data=q->data;
+         p->rchild=RDelete(p->rchild,q->data); 
+    }
+ } 
+p->height=NodeHeight(p);
+if(BalanceFactor(p)==2&&BalanceFactor(p->lchild)>=0){
+
+     printf("ll apllied\n");
+    return LLRotation(p);
+   
+}
+else if(BalanceFactor(p)==2&&BalanceFactor(p->lchild)==-1){
+     printf("lr apllied \n");
+   return LRRotation(p);
+}
+else if(BalanceFactor(p)==-2&&BalanceFactor(p->rchild)<=0){
+     printf("rr apllied \n");
+      return RRRotation(p);
+    
+}
+else if(BalanceFactor(p)==-2&&BalanceFactor(p->rchild)==1){
+     printf("rl apllied \n");
+    return RLRotation(p);
+}
+
+ return p;
+}
+
+
+
 void Inorder(struct Node *p){
     if(p){
         Inorder(p->lchild);
         printf("%d ", p->data);
         Inorder(p->rchild);
     }
+  
+}
+void PrintTree(struct Node*p,int space){
+    if(p==NULL)return;
+    space+=8;
+
+    PrintTree(p->rchild,space);
+    printf("\n");
+    int i;
+    for(i=8;i<space;i++){
+        printf(" ");
+
+    }
+ if(p== root){
+        printf("%d---------------------------------------------------------------------------------------------",p->data);
+    }else{
+    printf("%d\n",p->data);
+    }
+    printf("\n");
+
+    PrintTree(p->lchild,space);
 }
 
+void PrintSearch(struct Node*p,int key) {
+      while(p!=NULL){
+    if(key==p->data){
+        printf("%d is present in tree\n",key);
+        return;  /// very very important
+    }
+      if(key < p->data){
+       p=p->lchild;
+    }else{
+        p=p->rchild; 
+    }
+}
+ printf("%d is not present in tree\n",key);
+}
 
+struct Node *Search(struct Node*p,int key) {
+      while(p!=NULL){
+    if(key==p->data){
+        return p;
+    }
+      if(key < p->data){
+       p=p->lchild;
+    }else{
+        p=p->rchild; 
+    }
+}
+return NULL;
+}
 
+struct Node *Update(struct Node*p,int oldkey,int newkey) {
+if(Search(p,oldkey)==NULL){
+    printf("%d is not present in tree\n",oldkey); 
+    return p;
+}
+p=RDelete(p,oldkey);
+p=RInsert(p,newkey);
+return p;
+}
+int gcd(int a,int b){
+    if(b==0){
+        return a;
+    }
+     gcd(b,a%b);
+}
 
-
+int Gcd(int a,int b){
+    while(b!=0){
+        int temp=b;
+        b=a%b;
+        a=temp;
+    }
+    return a;
+}
 int main (){
- /*ll rotation
-  root=RInsert(root,30);
+ //ll rotation
+  /*root=RInsert(root,30);
   RInsert(root,20);
   RInsert(root,10);
   RInsert(root,5);
@@ -159,7 +299,30 @@ int main (){
   RInsert(root,28);
   RInsert(root,40);
   RInsert(root,50);
-Inorder(root);*/
+  RInsert(root,38);
+  RInsert(root,45);*/
+ // Inorder(root);
+// RDelete(root,28);
+//PrintSearch(root,30);  //METHOD1
+
+/*METHOD2 TO SEARCH
+struct Node *t;
+int key=80;
+t=Search(root,key);
+if(t){
+    printf("%d is present in tree\n",t->data);  
+}else{
+    printf("%d is not present in tree\n",key);  
+}
+*/
+
+// printf("BEFORE UPDATE\n");
+// Inorder(root);
+// printf("\n");
+// Update(root,25,35);
+// printf("AFTER UPDATE\n");
+// Inorder(root);
+// PrintTree(root,0);
 
 /*rr rotation
 root=RInsert(root,30);
@@ -176,9 +339,12 @@ RInsert(root,10);
 RInsert(root,20);
 Inorder(root);*/
 /*rl rotation*/
-root=RInsert(root,10);
+/*root=RInsert(root,10);
 RInsert(root,30);
 RInsert(root,20);
-Inorder(root);
+Inorder(root);*/
+
+printf("%d\n",Gcd(7,9));
+
     return 0;
 }
